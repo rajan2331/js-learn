4c3b7116768412ada5df7c561bc3f5919d8500eb
rajan2331 <rajansharma2331@gmail.com>
2024-01-02 18:24:56 +0000

    variable and datatype

---
+++ .devcontainer/basics/01_vars.js
@@ -0,0 +1,8 @@
+const bookId =12121
+var bookName = "subtle art of "
+let authorName = "Mark"
+ratings = 5
+bookId =3
+console.log(bookId);
+console.table([bookId , authorName, bookName ,ratings])
+// Donot use var sue to its block scope and func scope issue
---
+++ .devcontainer/basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+let age = 12
+let name ="rajan"
+let isAvailable = true;
+let object = String("rajan sharma")
+let object1 = Number(121)
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isAvailable)
+console.log(typeof object1)
+console.log( object1)
