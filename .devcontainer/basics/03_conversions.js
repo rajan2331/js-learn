b1fee88bfdc4aaf642fa54565ad05d1408c8ca51
rajan2331 <rajansharma2331@gmail.com>
2024-01-03 13:45:41 +0000

    operators

--- .devcontainer/basics/03_conversions.js
+++ .devcontainer/basics/03_conversions.js
@@ -1,11 +1,29 @@
 let score = false
 let scoreInString = String(score)
 let scoreInNum = Number(score)
-console.log(typeof score);
-console.log(typeof scoreInNum);
-console.log(scoreInNum)
+////console.log(typeof score);
+//console.log(typeof scoreInNum);
+//console.log(scoreInNum)
 let isLoggedIn = Boolean("")
-console.log(typeof isLoggedIn);
-console.log( isLoggedIn);
+//console.log(typeof isLoggedIn);
+//console.log( isLoggedIn);
 
 // careful as in JS type is always converted even though it might not contain the correct val as in case of num which if passed string will contain Nan
+
+/** conversionss -> operatrors */
+
+let str1 = 2
+let str2 = 3
+//console.log(str1 + str2 )
+//console.log(str1 - str2 )
+//console.log(str1 ** str2 ) // power 23 power 27
+//console.log(str1 * str2 )
+//console.log(str1 / str2 )
+//console.log(str1 % 47 )
+
+
+console.log("23"+ 2 + 3)
+console.log(2 + 3  + "23" )
+let num = 200
+console.log(num++)
+console.log(++num)
