65738489e13377f5a1c5b23cb10de61aca111702
rajan2331 <rajansharma2331@gmail.com>
2024-01-03 12:53:14 +0000

    conversion in js

---
+++ .devcontainer/basics/03_conversions.js
@@ -0,0 +1,11 @@
+let score = false
+let scoreInString = String(score)
+let scoreInNum = Number(score)
+console.log(typeof score);
+console.log(typeof scoreInNum);
+console.log(scoreInNum)
+let isLoggedIn = Boolean("")
+console.log(typeof isLoggedIn);
+console.log( isLoggedIn);
+
+// careful as in JS type is always converted even though it might not contain the correct val as in case of num which if passed string will contain Nan
